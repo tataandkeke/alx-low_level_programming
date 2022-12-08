500ecd969544a1d0ec52256fa41871f9f53fadca
richard <richardoladiran@gmail.com>
2022-12-08 06:25:42 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ *main - entry
+ *Return: return 0
+ **/ 
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
