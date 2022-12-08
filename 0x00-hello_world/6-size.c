fb048a74a932af6525773f26ad45566dde80797f
richard <richardoladiran@gmail.com>
2022-12-08 06:59:02 -0800

    Added

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 
 /**
  * main - program starts
- *
+ * Description: prints the sizes of type
  * Return 0 always
 */
 
@@ -12,6 +12,6 @@ int main(void)
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %d byte(s)", sizeof(float));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
