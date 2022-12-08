ee3a9ec4cfe01d305872dc56d8c7dab0834fe22d
richard <richardoladiran@gmail.com>
2022-12-08 06:07:08 -0800

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 
-int main ()
+int main(void)
 {
 	char str1[55];
 
-	strcpy(str1, "Programming is like building a multilingual puzzle");
+	strcpy(str1, "\"Programming is like building a multilingual puzzle");
 	puts(str1);
 
-	return(0);
+	return (0);
 }
