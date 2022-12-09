8e2d2ba55c24c6ddb7ffa2b66c3fd08fd0685698
richard <richardoladiran@gmail.com>
2022-12-09 06:30:00 -0800

    print 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+#include<time.h>
+
+/**
+*main - program starts
+*Description: prints all single digits from 0-9
+*Return: 0 all the time
+*/
+
+int main(void)
+{
+	char x = '0';
+
+	for (; x <= '9'; x++)
+	{
+		putchar(x);
+	}
+
+	putchar('\n');
+}
