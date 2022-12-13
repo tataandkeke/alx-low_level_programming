02704bd0f0c5c1092809f756e23200e156bffb39
richard <richardoladiran@gmail.com>
2022-12-13 09:02:31 -0800

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit of a number
+ * descriptions : prints all the lower case character
+ * @n: n return the number
+ * Return: + when more than zero, 0 when zero, - when negative
+ */
+int print_last_digit(int n)
+{
+	int last_digit = n % 10;
+
+	return (last_digit);
+}
