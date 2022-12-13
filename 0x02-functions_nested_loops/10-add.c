c939a4fbfef303de93c8935a78626f18369af0b1
richard <richardoladiran@gmail.com>
2022-12-13 10:50:40 -0800

    add 2 number

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * add - add 2 integers togehter
+ * descriptions : prints all the lower case character
+ * @a: first integer
+ * @b: second interger
+ * Return: + when more than zero, 0 when zero, - when negative
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}
