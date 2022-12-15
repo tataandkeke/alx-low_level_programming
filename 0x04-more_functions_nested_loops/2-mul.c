619f45f8b060fe3cf380052f9753fa9e853d066c
richard <richardoladiran@gmail.com>
2022-12-15 07:35:44 -0800

    multiply 2 numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiply 2 numbers
+ * @a: input
+ * @b: input 2
+ * Return: return 1 if digit, return 0 otherwise
+ */
+
+int mul(int a, int b)
+{
+	int sum = a * b;
+
+	return (sum);
+}
