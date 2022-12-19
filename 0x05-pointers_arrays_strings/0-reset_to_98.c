dd56b01ffa4bf80e991c53e71ac26a81fa491afd
richard <richardoladiran@gmail.com>
2022-12-19 03:13:03 -0800

    change vale to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+
+/**
+ * reset_to_98 - reset the value a pointer is refereing too
+ * @n: input pointer
+ * return:  return 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
