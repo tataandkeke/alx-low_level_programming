0ca1b86a272ed4b285e1742300d3c7e302207bd7
richard <richardoladiran@gmail.com>
2022-12-19 04:53:21 -0800

    reversing string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+
+/**
+ * rev_string - print in reverse
+ * @s: input pointer
+ * Return:  return 0
+ */
+
+void rev_string(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+
+	}
+
+	for (a--; a >= 0; a--)
+	{
+		_putchar(s[a]);
+	}
+
+}
