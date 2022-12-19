0b50e006b56b39dd750e28e6792a6209f2f99f62
richard <richardoladiran@gmail.com>
2022-12-19 04:32:23 -0800

    edited

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,6 +11,23 @@
 
 void print_rev(char *s)
 {
-	strrev(s);
-	printf("%s\n", s;
+	int a;
+	int start;
+	int end;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+
+	}
+
+	end = a - 1;
+
+	for (start = 0; start < a; start++)
+	{
+		s[start] = s[end];
+		end--;
+	}
+
+	s[start] = '\0';
+	printf("%s\n". s);
 }
