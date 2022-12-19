1406d94d3b13211914552a76d4d87b79979aa24b
richard <richardoladiran@gmail.com>
2022-12-19 04:20:58 -0800

    reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+
+/**
+ * print_rev - print in reverse
+ * @s: input pointer
+ * Return:  return 0
+ */
+
+void print_rev(char *s)
+{
+	printf("%s\n", strrev(s));
+}
