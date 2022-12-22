57a51de9d0fdb50229d742e0725ca244d147f600
richard <richardoladiran@gmail.com>
2022-12-22 08:03:57 -0800

    string copying

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * *_strncpy - join two string together
+ * @dest: input
+ * @src: input
+ * @n: input
+ * Return: returns a pointer
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a;
+
+	for (a = 0; a < n && src[a] != '\0'; a++)
+	{
+		dest[a] = src[a];
+	}
+
+	for (; a < n; b++)
+	{
+		dest[a] = '\0';
+	}
+
+	return (dest);
+}
