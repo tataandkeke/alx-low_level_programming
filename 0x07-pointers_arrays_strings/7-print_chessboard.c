ecd654dbcef489ab8d50d81f25d2ff76201c33a3
richard <richardoladiran@gmail.com>
2023-01-02 06:33:08 -0800

    chess

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *  print_chessboard - print chessboard
+ *  @a: 2d array
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int b, c;
+
+	b = 0;
+	while (b < 8)
+	{
+		c = 0;
+		while (c < 8)
+		{
+			_putchar(a[b][c]);
+			c++;
+		}
+		_putchar('\n');
+		b++;
+	}
+}
