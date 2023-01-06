f9cbe4678e78fec951fc6ee265cb7113c198e951
richard <richardoladiran@gmail.com>
2023-01-06 08:51:18 -0800

    add only positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * main - add positive numbers
+ * @argc: number of arguments passed
+ * @argv: array of strings
+ * Return: returns
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int addsum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	else if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (*argv[i] < 47 || *argv[i] > 57)
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+			{
+				addsum += atoi(argv[i]);
+			}
+		}
+		printf("%d\n", addsum);
+	}
+	return (0);
+}
