9182e9124de1e1cc49e939516327a5d4b1831e49
richard <richardoladiran@gmail.com>
2023-01-09 12:37:25 -0800

    advanced task 1

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenates all arguemts in the program
+ * @ac: argument counter
+ * @av: argument value
+ * Return: returns string
+ */
+
+char *argstostr(int ac, char **av)
+{
+	(void) ac;
+	(void) av;
+
+	return (0);
+}
