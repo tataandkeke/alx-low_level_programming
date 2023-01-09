25a1472a9f0fc9d4e3d4f0af42975b3db368b6fd
richard <richardoladiran@gmail.com>
2023-01-09 12:43:49 -0800

    edit

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * strtow - splits the string into words
+ * @str: string vale
+ * Return: returns string
+ */
+
+char **strtow(char *str)
+{
+	(void) str;
+
+	return (0);
+}
