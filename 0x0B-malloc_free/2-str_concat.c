0331e52b661dbbc124cdc58d29cee552ac53b89f
richard <richardoladiran@gmail.com>
2023-01-09 09:08:05 -0800

    de

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -37,15 +37,18 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 	}
 
-	for (i = 0; i < str_len1; i++)
+	for (i = 0; s1[i] != '\0'; i++)
 	{
-		newstring[i] = s1[i];
-	}
 
-	for (j = 0; j < str_len2; j++)
-	{
-		newstring[i - 1] = s2[j];
 	}
+	for (j = 0; s2[j] != '\0'; j++)
+	{
+		s1[i] = s2[j];
+		i++;
+	}
+	s1[i] = '\0';
+
+	newstring = s1;
 
 	return (newstring);
 }
