3ee326f0c2df5c9c1d423c799b5938a8441e74ec
richard <richardoladiran@gmail.com>
2023-01-09 08:40:07 -0800

    combining 2 strings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * str_concat - combines two strings together
+ * @s1: first string
+ * @s2: second string
+ * Return: Return NULL if it fails
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int i;
+	int j;
+
+	int str_len1;
+	int str_len2;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	str_len1 = strlen(s1);
+	str_len2 = strlen(s2);
+	total_len = str_len1 + str_len2;
+
+	char newstring = (char *)malloc(sizeof(char) * total_len + 2);
+
+	if (newstring == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < str_len1; i++)
+	{
+		newstring[i] = s1[i];
+	}
+
+	for (j = 0; j < str_len2; j++)
+	{
+		newstring[str_len1 + 1] = s2[j];
+	}
+
+	return (newstring);
+}
