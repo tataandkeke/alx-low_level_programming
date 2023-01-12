fad1b8c1b5fe1355afb4150042fdc5c4c44ec12d
richard <richardoladiran@gmail.com>
2023-01-12 11:26:54 -0800

    edit

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _realloc - reallocate a new memory
+ * @ptr: pointer
+ * @old_size: old memory size
+ * @new_size: new memory size to allocate
+ * Return: return
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	(void)ptr;
+	(void)old_size;
+	(void)new_size;
+
+	return (0);
+}
