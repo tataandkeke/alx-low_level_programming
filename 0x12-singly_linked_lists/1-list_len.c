7a462b259c36c0ffc9ccf2714c5cc3c8941d8f4c
richard <richardoladiran@gmail.com>
2023-01-26 08:10:24 -0800

    edited func

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -13,20 +13,10 @@ size_t list_len(const list_t *h);
 {
 	size_t count = 0;
 
-	if (h == NULL)
-	{
-		return (0);
-	}
-
 	while (h != NULL)
 	{
-		count++;
-		if (h->str == NULL)
-		{
-			printf("[0] (nil)\n");
-		}
-		printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
+		count++;
 	}
 
 	return (count);
