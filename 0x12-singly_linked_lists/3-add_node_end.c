bb9166e91859a3a6661f423430418ff7e41a35d1
richard <richardoladiran@gmail.com>
2023-01-26 08:48:57 -0800

    edited agin

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -13,7 +13,11 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node;
+	list_t *tmp;
+
 	unsigned int i;
+
+	tmp = *head;
 
 	for (i = 0; str[i] != '\0'; i++)
 		;
@@ -28,8 +32,17 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	new_node->len = i;
 	new_node->str = strdup(str);
-	(*head)->next = new_node;
+	new_node->next = NULL;
 
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
 
+	while (tmp->next)
+		tmp = tmp->next;
+
+	tmp->next = new_node;
 	return (new_node);
 }
