144e90a6c71b8f2b768dea8ed55406e2da5a8524
richard <richardoladiran@gmail.com>
2023-01-31 08:37:06 -0800

    edited

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -7,7 +7,7 @@
  * Return: number of nodes
  */
 
-listint_t *add_nodeint_end(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *tmp;
 	listint_t *new_node;
