d03b09d141bdbca32ba7da14b1ac8565264e18e5
richard <richardoladiran@gmail.com>
2023-02-02 06:13:28 -0800

    edit

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_binary - prints binary
+ * @n: number
+ * Return: result
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int current;
+	int i;
+	int count = 0;
+
+	for (i = 63; i >= 0; i--)
+	{
+		current = n >> i;
+
+		if (current & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+	if (!count)
+		_putchar('0');
+}
