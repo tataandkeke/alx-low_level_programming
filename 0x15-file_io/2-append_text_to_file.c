3318459ab958c84b653f394651e9987e766d9ab1
richard <richardoladiran@gmail.com>
2023-02-06 07:14:48 -0800

    created a file and appended to it

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * append_text_to_file- Read a text file
+ * @filename: char string
+ * @text_content: character to append
+ * Return: number of letter
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file_open, w, i;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+		return (1);
+
+	file_open = open(filename, O_RDWR);
+	if (file_open == -1)
+		return (-1);
+	for (i = 0; text_content[i] != '\0'; i++)
+		;
+
+	w = write(file_open, text_content, i);
+	if (w == -1)
+		return (-1);
+
+	close(file_open);
+	return (1);
+}
