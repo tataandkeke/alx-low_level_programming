f2aa8e8479e32ec0438c0cf3c60d9f15e069bd2b
richard <richardoladiran@gmail.com>
2023-02-06 07:18:19 -0800

    changed file permissions

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -17,13 +17,12 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		return (1);
 
-	file_open = open(filename, O_RDWR);
+	file_open = open(filename, O_APPEND | O_WRONLY);
 	if (file_open == -1)
 		return (-1);
 	for (i = 0; text_content[i] != '\0'; i++)
 		;
 
-	w = write(file_open, text_content, i);
 	if (w == -1)
 		return (-1);
 
