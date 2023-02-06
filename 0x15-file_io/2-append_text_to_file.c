0a838500841c18176e8d6b7435d9fecc249c1c86
richard <richardoladiran@gmail.com>
2023-02-06 07:20:15 -0800

    edit mistake

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,9 +20,11 @@ int append_text_to_file(const char *filename, char *text_content)
 	file_open = open(filename, O_APPEND | O_WRONLY);
 	if (file_open == -1)
 		return (-1);
+
 	for (i = 0; text_content[i] != '\0'; i++)
 		;
 
+	w = write(file_open, text_content, i);
 	if (w == -1)
 		return (-1);
 
