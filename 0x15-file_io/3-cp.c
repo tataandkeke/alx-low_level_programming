b121285a8471e08cf37db7c94848bb320de75c5e
richard <richardoladiran@gmail.com>
2023-02-06 07:23:53 -0800

    basic code

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - main code
+ * @ac: agcount
+ * @av: argumet vale
+ * Return: 0
+ */
+
+int main(int ac, char *av[])
+{
+	(void)ac;
+	(void)av;
+
+	return (0);
+}
